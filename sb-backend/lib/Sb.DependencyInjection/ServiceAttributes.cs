83d6929ac6c84d050a0dc2b2ec7ec3448a2a4cca
jrguth <jguth@silverwork-solutions.com>
2021-12-02 18:18:25 -0600

    add dependency injection module

---
+++ sb-backend/lib/Sb.DependencyInjection/ServiceAttributes.cs
@@ -0,0 +1,16 @@
+﻿namespace Sb.DependencyInjection;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class TransientServiceAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class ScopedServiceAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class SingletonServiceAttribute : Attribute
+{
+}
---
+++ sb-backend/lib/Sb.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+using Sb.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSbServicesFromAssembly(this IServiceCollection services)
+    {
+        return services.Scan(scan => scan
+            .FromCallingAssembly()
+                .AddClasses(@class => @class
+                    .WithAttribute<SingletonServiceAttribute>())
+                        .AsSelf()
+                        .WithSingletonLifetime()
+                .AddClasses(@class => @class
+                    .WithAttribute<TransientServiceAttribute>())
+                        .AsSelfWithInterfaces()
+                        .WithTransientLifetime()
+                .AddClasses(@class => @class
+                    .WithAttribute<ScopedServiceAttribute>())
+                        .AsSelfWithInterfaces()
+                        .WithScopedLifetime());
+    }
+}
